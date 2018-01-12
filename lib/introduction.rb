2cd9fab65d72ff3525ab6070f5e536689c71b929
Ben Matare <ben.a.matare@gmail.com>
2018-01-12 01:40:48 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,7 +1,7 @@
 def introduction(name)
-  puts "Hi, my name is #{name}"
+  puts "Hi, my name is #{name}."
 end
 
 def introduction_with_language(name,language)
-  puts "Hi, my name is #{name}, I speak #{language}"
+  puts "Hi, my name is #{name}, I am learning to program in #{language}."
 end
